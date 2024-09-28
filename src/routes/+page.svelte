571a44bb75d1c15aefd25a148c3b690fabe6282b
Kristian Michel <113218552+Bombachicky@users.noreply.github.com>
2024-09-28 15:07:38 -0400

    frontend purple scheme and red circle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import "@fontsource/advent-pro";
+  import Area from "./Area.svelte";
+</script>
+
+<div id="title">Lootcode 10</div>
+<div id="content">
+  <div id="map-container">
+    <img src="/map.jpg" alt="Lootcode Map" id="lootcode-map" />
+    <Area message="" vertical={40} horizontal={51.5}></Area>
+  </div>
+</div>
+
+<style>
+
+  #title {
+    text-align: center;
+    color: white;
+    font-size: 48px;
+    margin-top: 128px;
+    font-family: 'Advent Pro', sans-serif;
+  }
+
+  #lootcode-map {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border-radius: 15px; 
+  }
+
+</style>
---
+++ src/routes/Area.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    export let message: string;
+    export let vertical: number;
+    export let horizontal: number;
+  </script>
+  
+  <div 
+    id="circle"
+    style:top="{vertical}%"
+    style:left="{horizontal}%"
+  >
+    {message}
+  </div>
+  
+  <style>
+    #circle {
+      position: absolute;
+      width: 30px;
+      height: 30px;
+      background-color: #ef4444;
+      border: 1px solid white;
+      border-radius: 50%;
+      cursor: pointer;
+      transition: border-width 0.15s ease-in-out;
+      box-sizing: border-box; /* Include this to prevent size changes */
+    }
+    
+    #circle:hover {
+      border-width: 4px;
+    }
+  </style>
+  
