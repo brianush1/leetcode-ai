fca915d07c5516921273c2df3d6c7b018754bdd9
Kristian Michel <113218552+Bombachicky@users.noreply.github.com>
2024-09-28 15:58:52 -0400

    finished map page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,14 @@
   <div id="content">
     <div id="map-container">
       <img src="/map.jpg" alt="Lootcode Map" id="lootcode-map" />
-      <Area message="" vertical={40} horizontal={52.5}></Area>
+      <Area message="Sorting/Searching" vertical={40} horizontal={52.5}></Area>
+      <Area message="Strings" vertical={40} horizontal={90}></Area>
+      <Area message="DP" vertical={60} horizontal={64}></Area>
+      <Area message="Graph Algorithms" vertical={80} horizontal={8}></Area>
+      <Area message="Range Queries" vertical={30} horizontal={12}></Area>
+      <Area message="Trees" vertical={70} horizontal={27}></Area>
+      <Area message="Math" vertical={50} horizontal={20}></Area>
+      <Area message="Geometry" vertical={10} horizontal={35}></Area>
     </div>
   </div>
 </div>
--- src/routes/Area.svelte
+++ src/routes/Area.svelte
@@ -5,16 +5,23 @@
   </script>
   
   <div 
-    id="circle"
+    id="marker"
     style:top="{vertical}%"
     style:left="{horizontal}%"
   >
-    {message}
+    <div id="circle"></div>
+    <div id="label">{message}</div>
   </div>
   
   <style>
-    #circle {
+    /* Marker container positioned absolutely */
+    #marker {
       position: absolute;
+      /* No additional positioning needed here */
+    }
+  
+    /* Circle styles */
+    #circle {
       width: 30px;
       height: 30px;
       background-color: #ef4444;
@@ -22,11 +29,33 @@
       border-radius: 50%;
       cursor: pointer;
       transition: border-width 0.15s ease-in-out;
-      box-sizing: border-box; /* Include this to prevent size changes */
+      box-sizing: border-box;
     }
     
     #circle:hover {
       border-width: 4px;
+    }
+  
+    /* Label (rectangle) styles */
+    #label {
+      position: absolute;
+      top: 35px;
+      left: -55px;
+      width: 140px;
+      height: 28px;
+      background-color: #15162C;
+      border: 2px solid #a855f7;
+      border-radius: 5px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: white;
+      font-size: 16px;
+    }
+  
+    /* Show the label when hovering over the circle */
+    #circle:hover + #label {
+      opacity: 0.9;
     }
   </style>
   
