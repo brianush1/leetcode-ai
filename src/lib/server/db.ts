21bf738c6515c5f9b1e40c295cde9c3eaa3ce9da
brianush1 <brianush1@outlook.com>
2024-09-28 19:15:22 -0400

    add "database"

---
+++ src/lib/server/db.ts
@@ -0,0 +1,21 @@
+import { readFileSync, writeFileSync, existsSync } from "fs";
+
+export interface Database {
+	users: {
+		username: string;
+		password: string;
+	}[];
+}
+
+const defaultData: Database = {
+	users: [],
+};
+
+export const data: Database = {
+	...JSON.parse(JSON.stringify(defaultData)),
+	...JSON.parse(existsSync("data.json") ? readFileSync("data.json", "utf-8") : "{}"),
+};
+
+export function saveData() {
+	writeFileSync("data.json", JSON.stringify(data, undefined, "\t"));
+}
