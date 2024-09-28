b5c75e49dbbaa2bf5669ee35ce43e1c7f3c34cbb
farbubby <farhan.mahbub@ucf.edu>
2024-09-28 15:23:01 -0400

    Created problem component styling

--- src/routes/problem/+page.svelte
+++ src/routes/problem/+page.svelte
@@ -2,6 +2,8 @@
     import Problem from "../../components/problem.svelte";
     let title = "Problem 1"
     let description = "Dis problem is no easyyyyy"
+    let expectedInput = "Some input"
+    let expectedOutput = "Yahoo"
 </script>
 
-<div><Problem title={title} description={description}/></div>
+<div><Problem title={title} description={description} expectedInput={expectedInput} expectedOutput={expectedOutput}/></div>
